42f8f128de3b80070413bf180b270c84befa21b7
PickleIsDaBest <96940437+PickleIsDaBest@users.noreply.github.com>
2022-12-03 23:08:59 -0600

    Update Commands.lua

--- Commands.lua
+++ Commands.lua
@@ -40,20 +40,6 @@ cmds["rejoin"] = function(args, p)
             local plr = game.Players.LocalPlayer
 
             tpservice:Teleport(game.PlaceId, plr)
-         elseif msg:lower() == Settings['prefix']..'fling .' then
-                                power = 10000000 -- change this to make it more or less powerful
-
-                    game:GetService('RunService').Stepped:connect(function()
-                    game.Players.LocalPlayer.Character.Head.CanCollide = false
-                    game.Players.LocalPlayer.Character.UpperTorso.CanCollide = false
-                    game.Players.LocalPlayer.Character.LowerTorso.CanCollide = false
-                    game.Players.LocalPlayer.Character.HumanoidRootPart.CanCollide = false
-                    end)
-                    wait(.1)
-                    local bambam = Instance.new("BodyThrust")
-                    bambam.Parent = game.Players.LocalPlayer.Character.HumanoidRootPart
-                    bambam.Force = Vector3.new(power,0,power)
-                    bambam.Location = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
 end
 
 cmds["crash"] = function(args, p)
